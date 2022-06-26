ba2a17d16a749f5972a80193a98d2182a5cd18ae
Sujan Acharya <phoenix.sujan@gmail.com>
2022-06-26 20:53:09 +0400

    Added articles table, modified it with additional cols

---
+++ db/migrate/20220626164623_add_desc_ts_to_articles.rb
@@ -0,0 +1,7 @@
+class AddDescTsToArticles < ActiveRecord::Migration[7.0]
+  def change
+    add_column :articles, :description, :string
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+  end
+end
